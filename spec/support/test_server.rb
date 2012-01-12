e59eca0ea5de499ebec758447b97c7eed215fdfb
Sean Cribbs <sean@basho.com>
2012-01-12 14:17:36 -0500

    Fail faster if test server breaks, email only Sean on breakage.

--- spec/support/test_server.rb
+++ spec/support/test_server.rb
@@ -2,6 +2,7 @@ require 'riak/test_server'
 
 RSpec.configure do |config|
   config.before(:each, :integration => true) do
+    pending "Test server not working #{$test_server_fatal}" if $test_server_fatal
     begin
       unless $test_server
         config = YAML.load_file("spec/support/test_server.yml")
@@ -16,14 +17,19 @@ RSpec.configure do |config|
         $test_server.create unless $test_server.exist?
         $test_server.start
       end
+    rescue SocketError => e
+      warn "Couldn't connect to Riak TestServer! #{$test_server.inspect}"
+      warn "Skipping remaining integration tests."
+      $test_server_fatal = e
     rescue => e
       warn "Can't run integration specs without the test server. Please create spec/support/test_server.yml."
       warn e.inspect
+      $test_server_fatal = e
     end
   end
 
   config.after(:each, :integration => true) do
-    if $test_server && example.metadata[:test_server] != false
+    if $test_server && !$test_server_fatal && example.metadata[:test_server] != false
       $test_server.drop
     end
   end
