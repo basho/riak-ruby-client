bba8b1c81b7a38c8f72bd3e6162c0cd441093049
Sean Cribbs <sean@basho.com>
2012-04-11 13:22:56 -0400

    Use Rubygems' requirements logic instead of the existing kludge.

--- spec/support/version_filter.rb
+++ spec/support/version_filter.rb
@@ -1,14 +1,17 @@
+require 'rubygems'
+
 RSpec.configure do |config|
   config.before(:each, :integration => true,
                 :version => lambda {|v| !!v },
                 :test_server => lambda {|ts| ts != false }) do
     required = example.metadata[:version]
-    actual = test_server.version
+    actual = Gem::Version.new(test_server.version)
     case required
     when String
-      pending("SKIP: Tests feature for Riak #{required}, but testing against #{actual}") if actual < required
+      required = Gem::Requirement.create(">= #{required}")
     when Range
-      pending("SKIP: Tests feature for Riak versions #{required.begin} through #{required.end}, but testing against #{actual}") unless required.include?(actual)
+      required = Gem::Requirement.create([">= #{required.begin}", "<= #{required.end}"])
     end
+    pending("SKIP: Tests feature for Riak #{required.to_s}, but testing against #{actual.to_s}") unless required.satisfied_by?(actual)
   end
 end
