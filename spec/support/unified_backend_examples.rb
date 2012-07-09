23651c82ef815c0455cf1d94e8350bd5c16ac47c
Sean Cribbs <sean@basho.com>
2012-07-09 14:10:42 -0400

    Add failing specs for search integration.

--- spec/support/unified_backend_examples.rb
+++ spec/support/unified_backend_examples.rb
@@ -1,4 +1,3 @@
-
 shared_examples_for "Unified backend API" do
   # ping
   it "should ping the server" do
@@ -301,6 +300,43 @@ shared_examples_for "Unified backend API" do
         end
         errors.should be_empty
       end
+    end
+  end
+
+  # search
+  context "searching fulltext indexes", :focus => true do
+    before do
+      @bucket = @client.bucket('search_test')
+      orig_proto, @client.protocol = @client.protocol, :http
+      @bucket.enable_index!
+      @client.protocol = orig_proto      
+      idx = 0
+      IO.foreach("spec/fixtures/munchausen.txt") do |para|
+        next if para =~ /^\s*$|introduction|chapter/i
+        idx += 1
+        Riak::RObject.new(@bucket, "munchausen-#{idx}") do |obj|
+          obj.content_type = 'text/plain'
+          obj.raw_data = para
+          @backend.store_object(obj)
+        end
+      end
+    end
+
+    it 'should find indexed documents, returning ids' do
+      results = @backend.search 'search_test', 'fearless elephant rushed', :fl => 'id'
+      results.should have_key 'docs'
+      results.should have_key 'max_score'
+      results.should have_key 'num_found'
+      results['docs'].should include({"id" => "munchausen-605"})
+    end
+
+    it 'should find indexed documents, returning documents' do
+      # For now use '*' until #122 is merged into riak_search
+      results = @backend.search 'search_test', 'fearless elephant rushed', :fl => '*'
+      results.should have_key 'docs'
+      results.should have_key 'max_score'
+      results.should have_key 'num_found'
+      results['docs'].should include({"id" => "munchausen-605", "value" => "Fearless I advanced against the elephant, desirous to take alive the haughty Tippoo Sahib; but he drew a pistol from his belt, and discharged it full in my face as I rushed upon him, which did me no further harm than wound my cheek-bone, which disfigures me somewhat under my left eye. I could not withstand the rage and impulse of that moment, and with one blow of my sword separated his head from his body.\n"})
     end
   end
 end
