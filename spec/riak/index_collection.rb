dd392b2784debd0b3c2bf598ddddf5a88a64f3d5
Bryce Kerley <bkerley@brycekerley.net>
2013-06-25 15:03:34 -0400

    initialization

---
+++ spec/riak/index_collection.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Riak::IndexCollection do
+  describe "json initialization" do
+    it "should accept a list of keys"
+    it "should accept a list of keys and a continuation"
+    it "should accept a list of results hashes"
+    it "should accept a list of results hashes and a continuation"
+  end
+end
