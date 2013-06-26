161e6614a566d325c8ae59c85ba4a7abc3abf305
Bryce Kerley <bkerley@brycekerley.net>
2013-06-26 10:39:25 -0400

    Add some documentation on IndexCollection.

--- lib/riak/index_collection.rb
+++ lib/riak/index_collection.rb
@@ -1,7 +1,20 @@
 module Riak
+
+  # IndexCollection provides extra tools for managing index matches returned by
+  # a Secondary Index query. In Riak 1.4, these queries can be paginaged, and
+  # match keys up with the index values they matched against.
   class IndexCollection < Array
+
+    # @return [String] The continuation used to retrieve the next page of a
+    # paginated query.
     attr_reader :continuation
+
+    # @return [Hash<Integer/String, String>] A hash of index keys (String or
+    # Integer, depending on whether the query was a binary or integer) to
+    # arrays of keys.
     attr_reader :with_terms
+
+    # Create an IndexCollection from a JSON string.
     def initialize(json)
       parsed = JSON.parse json
       if parsed['keys']
