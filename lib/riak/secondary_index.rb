a252d88af639ef09ab896a8e1ba3f460bf89730c
Bryce Kerley <bkerley@brycekerley.net>
2013-06-27 16:59:17 -0400

    Error messages for missing 2i features.

--- lib/riak/secondary_index.rb
+++ lib/riak/secondary_index.rb
@@ -2,6 +2,7 @@ require 'riak/index_collection'
 module Riak
   class SecondaryIndex
     include Util::Translation
+    include Client::FeatureDetection
 
     # Create a Riak Secondary Index operation
     # @param [Bucket] the {Riak::Bucket} we'll query against
@@ -14,6 +15,8 @@ module Riak
       @index = index
       @query = query
       @options = options
+
+      validate_options
     end
 
     # Start the 2i fetch operation
@@ -35,12 +38,25 @@ module Riak
 
     # Get a new SecondaryIndex fetch for the next page
     def next_page
-      raise t('no_pagination_data') unless keys.continuation
+      raise t('index.no_next_page') unless keys.continuation
 
       self.class.new(@bucket, 
                      @index, 
                      @query, 
                      @options.merge(:continuation => keys.continuation))
+    end
+
+    private
+    def validate_options
+      raise t('index.pagination_not_available') if paginated? && !index_pagination?
+      raise t('index.return_terms_not_available') if @options[:return_terms] && !index_return_terms?
+
+      # TODO: uncomment that last part when implementing streaming
+      raise t('index.streaming_not_available') if @options[:stream] # && !index_streaming
+    end
+
+    def paginated?
+      @options[:continuation] || @options[:max_results]
     end
   end
 end
