2eb901473f02bc6799f04e128272a8a81b974f00
Bryce Kerley <bkerley@brycekerley.net>
2013-09-12 20:40:20 -0400

    Simplify and modernize the fulltext search encoding spec

--- spec/integration/riak/protobuffs_backends_spec.rb
+++ spec/integration/riak/protobuffs_backends_spec.rb
@@ -16,16 +16,14 @@ describe "Protocol Buffers", test_client: true do
 
         it_should_behave_like "Unified backend API"
 
-        if "".respond_to?(:encoding) # Ruby 1.9 and later only
-          describe "searching fulltext indexes (1.2 and later)", :version => '>= 1.2.0' do
-            include_context "search corpus setup"
+        describe "searching yokozuna" do
+          include_context "search corpus setup"
 
-            it 'should return documents with UTF-8 fields (GH #75)' do
-              utf8 = Encoding.find('UTF-8')
-              results = @backend.search 'search_test', 'fearless elephant rushed'
-              results['docs'].each do |d|
-                d.each {|(k,v)| k.encoding.should == utf8; v.encoding.should == utf8 }
-              end
+          it 'should return documents with UTF-8 fields (GH #75)' do
+            utf8 = Encoding.find('UTF-8')
+            results = @backend.search @search_bucket.name, 'fearless elephant rushed'
+            results['docs'].each do |d|
+              d.each {|(k,v)| k.encoding.should == utf8; v.encoding.should == utf8 }
             end
           end
         end
