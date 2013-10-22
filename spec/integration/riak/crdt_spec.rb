9003a6ab1a8d5940ddf20c6d502e87224600e147
Bryce Kerley <bkerley@brycekerley.net>
2013-10-22 16:22:03 -0400

    build out inner-map specs

--- spec/integration/riak/crdt_spec.rb
+++ spec/integration/riak/crdt_spec.rb
@@ -17,10 +17,21 @@ describe "CRDTs", integration: true, test_client: true do
   describe 'maps' do
     it 'should allow straightforward map ops'
     it 'should allow batched map ops'
-    describe 'containing maps' do
+    
+    describe 'containing a map' do
       it 'should bubble straightforward map ops up'
       it 'should bubble inner-map batches up'
       it 'should include inner-map ops in the outer-map batch'
+    end
+
+    describe 'containing a register' do
+      it 'should bubble straightforward register ops up'
+      # registers don't have batch ops
+    end
+
+    describe 'containing a flag' do
+      it 'should bubble straightforward flag ops up'
+      # flags don't have batch ops
     end
   end
 end
