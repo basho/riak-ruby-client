6a946b009168f22077b44f2a063a4f55ad28ffe0
Bryce Kerley <bkerley@brycekerley.net>
2013-10-25 10:46:12 -0400

    start speccing out typedcollection

--- spec/riak/crdt/typed_collection_spec.rb
+++ spec/riak/crdt/typed_collection_spec.rb
@@ -2,33 +2,39 @@ require 'spec_helper'
 
 describe Riak::Crdt::TypedCollection do
   describe 'initialization' do
-    it "should accept a type"
-    it "should accept a hash of values"
+    it "should accept a type" do
+      expect{ described_class.new Riak::Crdt::Counter }.to_not raise_error
+    end
+    it "should accept a hash of values" do
+      expect{ described_class.new Riak::Crdt::Counter, {} }.to_not raise_error
+    end
   end
 
   describe 'containing' do
     describe 'registers' do
-      should 'expose them as strings'
-      should 'update them'
+      it 'should expose them as strings'
+      it 'should send a MapOp with an update to the parent on update'
+      it 'should send a MapOp with an add and an update to the parent on create'
+      it 'should send a MapOp with a remove on remove'
     end
     describe 'flags' do
-      should 'expose them as booleans'
-      should 'update them'
+      it 'should expose them as booleans'
+      it 'should update them'
     end
     describe 'counters' do
-      should 'expose existing ones as Counter instances'
-      should 'expose new ones as Counter instances'
-      should 'allow incrementing and decrementing'
+      it 'should expose existing ones as Counter instances'
+      it 'should expose new ones as Counter instances'
+      it 'should allow incrementing and decrementing'
     end
     describe 'sets' do
-      should 'expose existing ones as Set instances'
-      should 'expose new ones as empty Set instances'
-      should 'allow adding and removing'
+      it 'should expose existing ones as Set instances'
+      it 'should expose new ones as empty Set instances'
+      it 'should allow adding and removing'
     end
     describe 'maps' do
-      should 'expose existing ones as populated Map instances'
-      should 'expose new ones as empty Map instances'
-      should 'cascade operations to a parent map'
+      it 'should expose existing ones as populated Map instances'
+      it 'should expose new ones as empty Map instances'
+      it 'should cascade operations to a parent map'
     end
   end
 end
