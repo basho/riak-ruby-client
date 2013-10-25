f6d41dc9d4b88609eb89068c6661180526fa7161
Bryce Kerley <bkerley@brycekerley.net>
2013-10-25 10:46:21 -0400

    typedcollection passes the first couple specs

--- lib/riak/crdt/typed_collection.rb
+++ lib/riak/crdt/typed_collection.rb
@@ -1,7 +1,10 @@
 module Riak
   module Crdt
     class TypedCollection
-      
+      def initialize(type, contents={})
+        @type = type
+        @contents = contents
+      end
     end
   end
 end
