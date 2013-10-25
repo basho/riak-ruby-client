1b08c5ac956ac5e4cd3501819baff89718e256a6
Bryce Kerley <bkerley@brycekerley.net>
2013-10-25 10:59:50 -0400

    implement hash behavior

--- lib/riak/crdt/typed_collection.rb
+++ lib/riak/crdt/typed_collection.rb
@@ -3,7 +3,13 @@ module Riak
     class TypedCollection
       def initialize(type, contents={})
         @type = type
-        @contents = contents
+        @contents = contents.stringify_keys
+      end
+
+      def [](key)
+        key = key.to_s
+        return @contents[key] if @contents.has_key? key
+        return @type.new
       end
     end
   end
