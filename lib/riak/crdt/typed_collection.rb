ed59dae58f09f950020836ca47b754e2ef6326f7
Bryce Kerley <bkerley@brycekerley.net>
2013-11-14 11:54:47 -0500

    implement update

--- lib/riak/crdt/typed_collection.rb
+++ lib/riak/crdt/typed_collection.rb
@@ -5,9 +5,10 @@ module Riak
         @type = type
         @parent = parent
         stringified_contents = contents.stringify_keys
-        @contents = stringified_contents.keys.inject(Hash.new) do |mem, key|
-          mem[key] = @type.new self, stringified_contents[key]
-          mem
+        @contents = stringified_contents.keys.inject(Hash.new) do |contents, key|
+          contents.tap do |c|
+            c[key] = @type.new self, stringified_contents[key]
+          end
         end
       end
 
@@ -20,14 +21,19 @@ module Riak
         return @contents[key] if include? key
         return @type.new
       end
+
+      def []=(key, value)
+        key = normalize_key key
+
+        operation = @contents[key].update value
+        operation.name = key
+
+        @parent.operate operation
+      end
       
       private
       def normalize_key(unnormalized_key)
         unnormalized_key.to_s
-      end
-            
-      def backend_class
-        @parent.backend_class
       end
     end
   end
