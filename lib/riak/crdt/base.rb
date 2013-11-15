08138faa10200b7ee9f187d7b194a61d1457a410
Bryce Kerley <bkerley@brycekerley.net>
2013-11-15 17:18:39 -0500

    gut a bunch of wrong-headed backend coupling

--- lib/riak/crdt/base.rb
+++ lib/riak/crdt/base.rb
@@ -7,17 +7,6 @@ module Riak
         @bucket_type = bucket_type
         @options = options
       end
-
-      def reload
-        response = client.backend do |be|
-          be.fetch_crdt @bucket, @key, @bucket_type, @options
-        end
-        @result = response.value
-      end
-
-      def backend_class
-        backend.class
-      end
       
       private
       def result
@@ -32,10 +21,6 @@ module Riak
 
       def backend
         client.backend{|be| be}
-      end
-
-      def send_operation(operation, options={})
-        backend.update_crdt @bucket, @key, @bucket_type, operation, options
       end
     end
   end
