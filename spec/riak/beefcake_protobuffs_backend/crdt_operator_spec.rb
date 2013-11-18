d7acc3bcdf60d1290220fc29a02e6715b3e27209
Bryce Kerley <bkerley@brycekerley.net>
2013-11-18 17:24:00 -0500

    stub out specs for OperationRecorder and Beefcake::CrdtOperator

---
+++ spec/riak/beefcake_protobuffs_backend/crdt_operator_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Riak::Client::BeefcakeProtobuffsBackend::CrdtOperator do
+  it 'should consume an OperationRecorder and serialize operations into protobuffs'
+end
---
+++ spec/riak/crdt/operation_recorder_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Riak::Crdt::OperationRecorder do
+  it 'should accept top-level operations'
+  it 'should buffer operations by default'
+  it 'should optimize multiple operations'
+end
