0c24533ce88248f04afcc6e725562f5ee21e2062
Bryce Kerley <bkerley@brycekerley.net>
2013-11-26 14:33:08 -0500

    Serialize set operations

--- lib/riak/client/beefcake/crdt_operator.rb
+++ lib/riak/client/beefcake/crdt_operator.rb
@@ -6,6 +6,8 @@ module Riak
           case operation.type
           when :counter
             return serialize_counter operation
+          when :set
+            return serialize_set operation
           end
         end
 
@@ -16,6 +18,17 @@ module Riak
                    counter_op: CounterOp.new(
                                              increment: counter_op.value
                                              )
+                   )
+        end
+
+        def serialize_set(set_op)
+          value = set_op.value or nil
+          
+          DtOp.new(
+                   set_op: SetOp.new(
+                                     adds: value[:add],
+                                     removes: value[:remove]
+                                     )
                    )
         end
       end
--- spec/riak/beefcake_protobuffs_backend/crdt_operator_spec.rb
+++ spec/riak/beefcake_protobuffs_backend/crdt_operator_spec.rb
@@ -6,12 +6,6 @@ describe Riak::Client::BeefcakeProtobuffsBackend::CrdtOperator do
   let(:backend_class){ Riak::Client::BeefcakeProtobuffsBackend }
 
   describe 'operating on a counter' do
-    let(:key){ 'test_counter'}
-    let(:backend){ double 'backend' }
-    let(:client){ double 'client' }
-    let(:bucket){ double 'bucket', name: 'counters', client: client }
-
-
     let(:increment){ 5 }
     let(:operation) do
       Riak::Crdt::Operation::Update.new.tap do |op|
@@ -28,8 +22,33 @@ describe Riak::Client::BeefcakeProtobuffsBackend::CrdtOperator do
       result = subject.serialize operation
 
       expect(result).to be_a backend_class::DtOp
-      expect(result.counter_op).to be
+      expect(result.counter_op).to be_a backend_class::CounterOp
       expect(result.counter_op.increment).to eq increment
+    end
+  end
+
+  describe 'operating on a set' do
+    let(:added_element){ 'added_element' }
+    let(:removed_element){ 'removed_element' }
+    let(:operation) do
+      Riak::Crdt::Operation::Update.new.tap do |op|
+        op.parent = double 'parent'
+        op.name = nil
+        op.type = :set
+        op.value = {
+          add: [added_element],
+          remove: [removed_element]
+        }
+      end
+    end
+
+    it 'should serialize a set operation into protobuffs' do
+      result = subject.serialize operation
+      
+      expect(result).to be_a backend_class::DtOp
+      expect(result.set_op).to be_a backend_class::SetOp
+      expect(result.set_op.adds).to eq [added_element]
+      expect(result.set_op.removes).to eq [removed_element]
     end
   end
 
