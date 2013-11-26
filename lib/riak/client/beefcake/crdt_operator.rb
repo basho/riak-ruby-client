6de50f7a10dfc797493fc6b17c75f8ab63a62141
Bryce Kerley <bkerley@brycekerley.net>
2013-11-26 14:24:05 -0500

    Actually serialize counter operations

--- lib/riak/client/beefcake/crdt_operator.rb
+++ lib/riak/client/beefcake/crdt_operator.rb
@@ -2,6 +2,22 @@ module Riak
   class Client
     class BeefcakeProtobuffsBackend
       class CrdtOperator
+        def serialize(operation)
+          case operation.type
+          when :counter
+            return serialize_counter operation
+          end
+        end
+
+        private
+        
+        def serialize_counter(counter_op)
+          DtOp.new(
+                   counter_op: CounterOp.new(
+                                             increment: counter_op.value
+                                             )
+                   )
+        end
       end
     end
   end
--- spec/riak/beefcake_protobuffs_backend/crdt_operator_spec.rb
+++ spec/riak/beefcake_protobuffs_backend/crdt_operator_spec.rb
@@ -2,6 +2,38 @@ require 'spec_helper'
 Riak::Client::BeefcakeProtobuffsBackend.configured?
 
 describe Riak::Client::BeefcakeProtobuffsBackend::CrdtOperator do
-  it 'should consume an OperationRecorder and serialize operations into protobuffs'
-  
+
+  let(:backend_class){ Riak::Client::BeefcakeProtobuffsBackend }
+
+  describe 'operating on a counter' do
+    let(:key){ 'test_counter'}
+    let(:backend){ double 'backend' }
+    let(:client){ double 'client' }
+    let(:bucket){ double 'bucket', name: 'counters', client: client }
+
+
+    let(:increment){ 5 }
+    let(:operation) do
+      Riak::Crdt::Operation::Update.new.tap do |op|
+        op.parent = double 'parent'
+        op.name = nil
+        op.type = :counter
+        op.value = increment
+      end
+    end
+    
+    subject { described_class.new }
+    
+    it 'should serialize a counter operation into protobuffs' do
+      result = subject.serialize operation
+
+      expect(result).to be_a backend_class::DtOp
+      expect(result.counter_op).to be
+      expect(result.counter_op.increment).to eq increment
+    end
+  end
+
+  describe 'operating on a map' do
+    it 'should consume an OperationRecorder and serialize operations into protobuffs'
+  end
 end
