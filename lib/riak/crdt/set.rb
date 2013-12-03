82e93bf62c8a837301a7ae25457e2c5791f694e3
Bryce Kerley <bkerley@brycekerley.net>
2013-12-03 17:07:04 -0500

    Set integration tests pass

--- lib/riak/crdt/set.rb
+++ lib/riak/crdt/set.rb
@@ -1,21 +1,39 @@
 module Riak
   module Crdt
     class Set < Base
+      
       def initialize(bucket, key, bucket_type=DEFAULT_SET_BUCKET_TYPE, options={})
         super(bucket, key, bucket_type, options)
       end
 
-      def members
-        return ::Set.new result.set_value unless result.nil? || result.set_value.nil?
-        return ::Set.new
+      def vivify(value)
+        @members = value
       end
 
+      def members
+        reload if dirty?
+        @members
+      end
+      
       def include?(candidate)
         members.include?(candidate)
       end
 
+      def add(element, options={})
+        operate operation(:add, element), options
+      end
 
+      def remove(element, options={})
+        operate operation(:remove, element), options
+      end
+      
       private
+      def operation(direction, element)
+        Operation::Update.new.tap do |op|
+          op.type = :set
+          op.value = { direction => element }
+        end
+      end
     end
   end
 end
