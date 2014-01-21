5ebad538d06da2efd71dff1c9ca4fec3cdb08dd7
Bryce Kerley <bkerley@brycekerley.net>
2014-01-21 15:40:31 -0500

    fix encoding issues when running specs with a us-ascii environment

--- spec/support/search_corpus_setup.rb
+++ spec/support/search_corpus_setup.rb
@@ -1,11 +1,14 @@
+# encoding: utf-8
 shared_context "search corpus setup" do
   before do
     @search_bucket = random_bucket 'search_test'
     @backend.create_search_index @search_bucket.name
     @search_bucket.props = {search_index: @search_bucket.name}
     idx = 0
+    old_encoding = Encoding.default_external
+    Encoding.default_external = Encoding::UTF_8
     IO.foreach("spec/fixtures/munchausen.txt") do |para|
-      next if para =~ /^\s*$|introduction|chapter/i
+      next if para =~ /^\s*$|introduction|chapter/ui
       idx += 1
       Riak::RObject.new(@search_bucket, "munchausen-#{idx}") do |obj|
         obj.content_type = 'text/plain'
@@ -14,6 +17,7 @@ shared_context "search corpus setup" do
       end
     end
 
+    Encoding.default_external = old_encoding
     sleep 1
   end
 end
