c964dbfdbcf8761c63f7c86ff961787c18cbe9fd
Bryce Kerley <bkerley@brycekerley.net>
2014-02-05 14:35:23 -0500

    Squelch an i18n warning

--- lib/riak/i18n.rb
+++ lib/riak/i18n.rb
@@ -1,5 +1,7 @@
 require 'i18n'
 
+I18n.config.enforce_available_locales = true unless I18n.config.enforce_available_locales == false
+
 Dir.glob(File.expand_path("../locale/*.yml", __FILE__)).each do |locale_file|
   I18n.load_path << locale_file
 end
