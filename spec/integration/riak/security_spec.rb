fff1370ea656318c11b8fdbc0c4310947bbd040c
Bryce Kerley <bkerley@brycekerley.net>
2014-02-05 14:36:08 -0500

    fill out security-enabled tests

--- spec/integration/riak/security_spec.rb
+++ spec/integration/riak/security_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require 'riak'
 
 describe 'Secure Protobuffs', test_client: true, integration: true do
+  let(:config){ test_client_configuration.dup }
   describe 'without security enabled on Riak', no_security: true do
     it 'should connect normally without authentication configured' do
       expect(test_client.security?).to_not be
@@ -10,7 +11,6 @@ describe 'Secure Protobuffs', test_client: true, integration: true do
     end
 
     it 'should refuse to connect with authentication configured' do
-      config = test_client_configuration.dup
       config[:authentication] = { username: 'user', password: 'password' }
 
       secure_client = Riak::Client.new config
@@ -21,14 +21,14 @@ describe 'Secure Protobuffs', test_client: true, integration: true do
 
   describe 'with security enabled on Riak', yes_security: true do
     it 'should connect normally with authentication configured' do
-      expect(test_client.security?).to be
+      secure_client = Riak::Client.new config
 
-      expect(test_client.ping).to be
+      expect(secure_client.security?).to be
+
+      expect{secure_client.ping}.to be
     end
 
     it 'should refuse to connect without authentication configured' do
-      config = test_client_configuration.dup
-
       config.delete :authentication
 
       plaintext_client = Riak::Client.new config
@@ -36,7 +36,16 @@ describe 'Secure Protobuffs', test_client: true, integration: true do
       expect{ plaintext_client.ping }.to raise_error
     end
 
-    it "should refuse to connect if the server cert isn't recognized"
+    it "should refuse to connect if the server cert isn't recognized" do
+      # this CA has never ever been used to sign a key
+      config[:authentication][:ca_file] =
+        File.join('support', 'certs', 'empty_ca.crt')
+
+      bugged_crypto_client = Riak::Client.new config
+
+      expect{ bugged_crypto_client.ping }.to raise_error
+    end
+
     it "should refuse to connect if the server cert is revoked"
   end
 end
