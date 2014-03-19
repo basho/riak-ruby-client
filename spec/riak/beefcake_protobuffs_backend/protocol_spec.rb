fd84a3ca14920249896f8176e6893b69e64795f3
Bryce Kerley <bkerley@brycekerley.net>
2014-03-18 21:38:09 -0400

    spec receiving messages and codes

--- spec/riak/beefcake_protobuffs_backend/protocol_spec.rb
+++ spec/riak/beefcake_protobuffs_backend/protocol_spec.rb
@@ -4,6 +4,8 @@ require 'riak/client/beefcake/protocol'
 describe Riak::Client::BeefcakeProtobuffsBackend::Protocol do
   let(:socket){ double 'Socket' }
   subject{ described_class.new socket }
+  let(:codes){ Riak::Client::BeefcakeMessageCodes }
+
   describe 'writing messages' do
     # I really wanted to call this "send" but Ruby already has that method
 
@@ -13,7 +15,7 @@ describe Riak::Client::BeefcakeProtobuffsBackend::Protocol do
       socket.should_receive(:write) do |payload|
         length, code = payload.unpack 'NC'
         expect(length).to eq 1
-        expect(code).to eq Riak::Client::BeefcakeMessageCodes.index name
+        expect(code).to eq codes.index name
       end.ordered
       socket.should_receive(:flush).ordered
 
@@ -21,15 +23,14 @@ describe Riak::Client::BeefcakeProtobuffsBackend::Protocol do
     end
 
     it 'writes messages with Beefcake::Message instances as bodies' do
-      message = Riak::Client::BeefcakeProtobuffsBackend::
-        RpbYokozunaSchemaGetReq.new name: 'schema'
+      message = yz_req
       name = :YokozunaSchemaGetReq
 
       socket.should_receive(:write) do |payload|
         header = payload[0..4]
         body = payload[5..-1]
         length, code = header.unpack 'NC'
-        expect(code).to eq Riak::Client::BeefcakeMessageCodes.index name
+        expect(code).to eq codes.index name
         expect(length).to eq(body.length + 1)
         expect(body).to eq message.encode.to_s
       end.ordered
@@ -40,8 +41,41 @@ describe Riak::Client::BeefcakeProtobuffsBackend::Protocol do
   end
 
   describe 'receiving messages' do
-    it 'receives messages without a body'
-    it 'receives messages and returns a Beefcake::Message instance'
+    it 'receives messages without a body and returns an array of code-symbol and nil' do
+      name = :PingResp
+      header = [1, codes.index(name)].pack 'NC'
+      socket.should_receive(:read).
+        ordered.
+        with(5).
+        and_return(header)
+
+      code, payload = subject.receive
+
+      expect(code).to eq name
+      expect(payload).to eq nil
+    end
+
+    it 'receives messages and returns an array of code-symbol and string' do
+      message = ctr_resp
+      message_str = message.encode.to_s
+      message_len = message_str.length
+      name = :CounterGetResp
+      header = [message_len + 1, codes.index(name)].pack 'NC'
+
+      socket.should_receive(:read).
+        ordered.
+        with(5).
+        and_return(header)
+      socket.should_receive(:read).
+        ordered.
+        with(message_len).
+        and_return(message_str)
+
+      code, payload = subject.receive
+      
+      expect(code).to eq name
+      expect(payload).to eq message_str
+    end
   end
 
   describe 'expecting messages' do
@@ -58,4 +92,9 @@ describe Riak::Client::BeefcakeProtobuffsBackend::Protocol do
       it 'raises a ProtobuffsErrorResponse error'
     end
   end
+
+  let(:yz_req){ Riak::Client::BeefcakeProtobuffsBackend::
+    RpbYokozunaSchemaGetReq.new name: 'schema' }
+  let(:ctr_resp){ Riak::Client::BeefcakeProtobuffsBackend::
+    RpbCounterGetResp.new value: rand(2**10) }
 end
