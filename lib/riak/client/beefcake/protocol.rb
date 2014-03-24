892f3e2888e6e34cef67fef97ea7e17469920454
Bryce Kerley <bkerley@brycekerley.net>
2014-03-24 12:56:47 -0400

    add documentation for the protocol class

--- lib/riak/client/beefcake/protocol.rb
+++ lib/riak/client/beefcake/protocol.rb
@@ -12,6 +12,13 @@ module Riak
           @socket = socket
         end
 
+        # Encodes and writes a Riak-formatted message, including protocol buffer
+        # payload if given.
+        # 
+        # @param [Symbol, Integer] code the symbolic or numeric code for the 
+        #   message
+        # @param [Beefcake::Message, nil] message the protocol buffer message
+        #   payload, or nil if the message carries no payload
         def write(code, message=nil)
           if code.is_a? Symbol
             code = BeefcakeMessageCodes.index code
@@ -27,6 +34,10 @@ module Riak
           socket.flush
         end
 
+        # Receives a Riak-formatted message, and returns the symbolic name of
+        # the message along with the string payload from the network.
+        #
+        # @return [Array<Symbol, String>]
         def receive
           header = socket.read 5
           
@@ -41,7 +52,23 @@ module Riak
           return name, body
         end
 
-        
+        # Receives a Riak-formatted message, checks the symbolic name against
+        # the given code, decodes it if it matches, and can optionally return
+        # success if the payload is empty.
+        #
+        # @param [Symbol] code the code for the message
+        # @param [Class, nil] decoder_class the class to attempt to decode 
+        #   the payload with
+        # @param [Hash] options
+        # @option options [Boolean] :empty_body_acceptable Whether to accept
+        #   an empty body and not attempt decoding. In this case, this method
+        #   will return the symbol `:empty` instead of a `Beefcake::Message`
+        #   instance
+        # @return [Beefcake::Message, :empty]
+        # @raise {ProtobuffsErrorResponse} if the message from Riak was a
+        #   255-ErrorResp
+        # @raise {ProtobuffsUnexpectedResponse} if the message from riak did
+        #   not match `code`
         def expect(code, decoder_class=nil, options={ })
           code = BeefcakeMessageCodes[code] unless code.is_a? Symbol
           name, body = receive
