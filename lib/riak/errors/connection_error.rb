1735f6481ab39b31a6e8799975dcd357a16b6c1d
Bryce Kerley <bkerley@brycekerley.net>
2014-04-18 11:02:43 -0400

    error and text for user vs. username in auth hash

--- lib/riak/errors/connection_error.rb
+++ lib/riak/errors/connection_error.rb
@@ -6,4 +6,10 @@ module Riak
 
   class TlsError < ConnectionError
   end
+
+  class UserConfigurationError < ConnectionError
+    def initialize
+      super t('pbc.user_not_username')
+    end
+  end
 end
