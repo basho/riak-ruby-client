e363eff8d5cf3583055619772dcb63b4fa65cac5
Bryce Kerley <bkerley@brycekerley.net>
2014-04-18 11:19:40 -0400

    fix spec names to be more idiomatic

--- spec/integration/riak/security_spec.rb
+++ spec/integration/riak/security_spec.rb
@@ -4,13 +4,13 @@ require 'riak'
 describe 'Secure Protobuffs', test_client: true, integration: true do
   let(:config){ test_client_configuration.dup }
   describe 'without security enabled on Riak', no_security: true do
-    it 'should connect normally without authentication configured' do
+    it 'connects normally without authentication configured' do
       expect(test_client.security?).to_not be
 
       expect(test_client.ping).to be
     end
 
-    it 'should refuse to connect with authentication configured' do
+    it 'refuses to connect with authentication configured' do
       config[:authentication] = { user: 'user', password: 'password' }
 
       secure_client = Riak::Client.new config
@@ -20,7 +20,7 @@ describe 'Secure Protobuffs', test_client: true, integration: true do
   end
 
   describe 'with security enabled on Riak', yes_security: true do
-    it 'should connect normally with authentication configured' do
+    it 'connects normally with authentication configured' do
       secure_client = Riak::Client.new config
 
       expect(secure_client.security?).to be
@@ -28,7 +28,7 @@ describe 'Secure Protobuffs', test_client: true, integration: true do
       expect{secure_client.ping}.to be
     end
 
-    it 'should refuse to connect without authentication configured' do
+    it 'refuses to connect without authentication configured' do
       config.delete :authentication
 
       plaintext_client = Riak::Client.new config
@@ -36,7 +36,7 @@ describe 'Secure Protobuffs', test_client: true, integration: true do
       expect{ plaintext_client.ping }.to raise_error
     end
 
-    it "should refuse to connect if the server cert isn't recognized" do
+    it "refuses to connect if the server cert isn't recognized" do
       # this CA has never ever been used to sign a key
       config[:authentication][:ca_file] =
         File.join('support', 'certs', 'empty_ca.crt')
@@ -46,6 +46,6 @@ describe 'Secure Protobuffs', test_client: true, integration: true do
       expect{ bugged_crypto_client.ping }.to raise_error
     end
 
-    it "should refuse to connect if the server cert is revoked"
+    it "refuses to connect if the server cert is revoked"
   end
 end
