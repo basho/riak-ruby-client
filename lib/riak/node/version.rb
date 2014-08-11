fd9df355569a69123fa0e900c1c688832c643205
Sean Cribbs <sean@basho.com>
2014-08-11 14:37:01 -0500

    Enhance base_dir detection so that it's not always assumed to be a source install.
    This should resolve issues on package installs where the base
    directory is not the parent of the bin directory, e.g. /usr/lib64/riak
    and /usr/sbin, respectively.

--- lib/riak/node/version.rb
+++ lib/riak/node/version.rb
@@ -21,9 +21,29 @@ module Riak
       end
     end
 
-    # Determines the base_dir from source parent
+    # Determines the base_dir from control script or source dir
     def configure_base_dir
-      source.parent
+      # Read the 'riak' script.
+      control_script_contents = (source + control_script_name).read
+      # See if it uses nodetool by sourcing env.sh, if so, read the
+      # env.sh instead, since it contains the setting of the base
+      # directory.
+      control_script_contents.match /\s*\.\s+"?(.*env\.sh)"?/ do |m|
+        control_script_contents = Pathname.new(m[1]).read
+      end
+      # Find the RUNNER_BASE_DIR in the script. If it has shell
+      # expansions in it, we're looking at a relative path and should
+      # assume this is a source or devrel installation, and thus use
+      # the parent directory as the base. Otherwise, if it's a regular
+      # path, we can assume it's absolute and pointing to the lib dir.
+      # This is where the 'releases' directory exists.
+      control_script_contents.match /RUNNER_BASE_DIR=(.*)/ do |m|
+        if m[1].include? "$"
+          source.parent
+        else
+          m[1].strip
+        end
+      end
     end
   end
 end
