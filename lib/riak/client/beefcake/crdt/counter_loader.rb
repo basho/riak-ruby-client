2e38d9c7bd2ab0411ef5d702a17f779b1cdb8f95
Bryce Kerley <bkerley@brycekerley.net>
2014-09-09 12:39:46 -0400

    smaller pyramids in crdt loader hierarchy

--- lib/riak/client/beefcake/crdt/counter_loader.rb
+++ lib/riak/client/beefcake/crdt/counter_loader.rb
@@ -1,21 +1,17 @@
-module Riak
-  class Client
-    class BeefcakeProtobuffsBackend
-      class CrdtLoader
-        class CounterLoader
-          def self.for_value(resp)
-            return nil unless resp.counter_value
-            new resp.counter_value
-          end
+class Riak::Client::BeefcakeProtobuffsBackend
+  class CrdtLoader
+    class CounterLoader
+      def self.for_value(resp)
+        return nil unless resp.counter_value
+        new resp.counter_value
+      end
 
-          def initialize(counter_value)
-            @value = counter_value
-          end
+      def initialize(counter_value)
+        @value = counter_value
+      end
 
-          def rubyfy
-            @value
-          end
-        end
+      def rubyfy
+        @value
       end
     end
   end
--- lib/riak/client/beefcake/crdt/map_loader.rb
+++ lib/riak/client/beefcake/crdt/map_loader.rb
@@ -1,67 +1,63 @@
-module Riak
-  class Client
-    class BeefcakeProtobuffsBackend
-      class CrdtLoader
-        class MapLoader
-          def self.for_value(resp)
-            return nil unless resp.map_value
-            new resp.map_value
-          end
+class Riak::Client::BeefcakeProtobuffsBackend
+  class CrdtLoader
+    class MapLoader
+      def self.for_value(resp)
+        return nil unless resp.map_value
+        new resp.map_value
+      end
 
-          def initialize(map_value)
-            @value = map_value
-          end
+      def initialize(map_value)
+        @value = map_value
+      end
 
-          def rubyfy
-            accum = {
-              counters: {},
-              flags: {},
-              maps: {},
-              registers: {},
-              sets: {}
-            }
+      def rubyfy
+        accum = {
+          counters: {},
+          flags: {},
+          maps: {},
+          registers: {},
+          sets: {}
+        }
 
-            contents_loop @value, accum
-          end
+        contents_loop @value, accum
+      end
 
-          private
+      private
 
-          def rubyfy_inner(accum, map_value)
-            destination = accum[:maps][map_value.field.name]
-            if destination.nil?
-              destination = accum[:maps][map_value.field.name] = {
-                counters: {},
-                flags: {},
-                maps: {},
-                registers: {},
-                sets: {}
-              }
-            end
+      def rubyfy_inner(accum, map_value)
+        destination = accum[:maps][map_value.field.name]
+        if destination.nil?
+          destination = accum[:maps][map_value.field.name] = {
+            counters: {},
+            flags: {},
+            maps: {},
+            registers: {},
+            sets: {}
+          }
+        end
 
-            contents_loop map_value.map_value, destination
-          end
+        contents_loop map_value.map_value, destination
+      end
 
-          def contents_loop(rolling_value, destination)
-            return destination if rolling_value.nil?
+      def contents_loop(rolling_value, destination)
+        return destination if rolling_value.nil?
 
-            rolling_value.each do |inner|
-              case inner.field.type
-              when MapField::MapFieldType::COUNTER
-                destination[:counters][inner.field.name] = inner.counter_value
-              when MapField::MapFieldType::FLAG
-                destination[:flags][inner.field.name] = inner.flag_value
-              when MapField::MapFieldType::MAP
-                rubyfy_inner destination, inner
-              when MapField::MapFieldType::REGISTER
-                destination[:registers][inner.field.name] = inner.register_value
-              when MapField::MapFieldType::SET
-                destination[:sets][inner.field.name] = ::Set.new inner.set_value
-              end
-            end
-
-            return destination
+        rolling_value.each do |inner|
+          case inner.field.type
+          when MapField::MapFieldType::COUNTER
+            destination[:counters][inner.field.name] = inner.counter_value
+          when MapField::MapFieldType::FLAG
+            destination[:flags][inner.field.name] = inner.flag_value
+          when MapField::MapFieldType::MAP
+            rubyfy_inner destination, inner
+          when MapField::MapFieldType::REGISTER
+            destination[:registers][inner.field.name] = inner.register_value
+          when MapField::MapFieldType::SET
+            destination[:sets][inner.field.name] = ::Set.new inner.set_value
           end
         end
+
+        return destination
       end
     end
   end
