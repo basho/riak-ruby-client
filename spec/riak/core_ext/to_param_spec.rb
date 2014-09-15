4b7a3503c5da1aa79098444f636d52b3e39a7d00
Bryce Kerley <bkerley@brycekerley.net>
2014-09-15 12:37:57 -0400

    client and to_param de-should

--- spec/riak/core_ext/to_param_spec.rb
+++ spec/riak/core_ext/to_param_spec.rb
@@ -2,13 +2,13 @@
 describe Riak do
   require 'riak/core_ext/to_param'
 
-  it "should do param conversion correctly" do
+  it "converts params correctly" do
     expect({ :name => 'David', :nationality => 'Danish' }.to_param).to eq("name=David&nationality=Danish")
   end
 
   # Based on the activesupport implementation.
   # https://github.com/rails/rails/blob/master/activesupport/lib/active_support/core_ext/object/to_param.rb
-  it "should do param conversion correctly with a namespace" do
+  it "converts namespaced params correctly" do
     expect({ :name => 'David', :nationality => 'Danish' }.to_param('user')).to eq("user%5Bname%5D=David&user%5Bnationality%5D=Danish")
   end
 
