7166d80bcf2421214916401f6700f22caf8cd897
Bryce Kerley <bkerley@brycekerley.net>
2014-09-30 15:16:52 -0400

    javascripts for table of contents

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,11 @@
 //= require_tree .
+
+jQuery(function($){
+  var tocContainer = $('#toc')
+
+  tocContainer.toc({
+    container: '#yielded',
+    smoothScrolling: false,
+    highlightOnScroll: false
+  });
+});
