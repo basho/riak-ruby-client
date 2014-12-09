4f054761e0c29511719edef47388137f20fe9489
Bryce Kerley <bkerley@brycekerley.net>
2014-12-09 09:56:53 -0500

    spike out search errors

---
+++ lib/riak/errors/search_error.rb
@@ -0,0 +1,17 @@
+requrie 'riak/errors/base'
+
+module Riak
+  class SearchError < Error
+    class IndexExistsError < SearchError
+      def initialize(nane)
+        super t('search.index_exists', name: name)
+      end
+    end
+
+    class SchemaExistsError < SearchError
+      def initialize(name)
+        super t('search.schema_exists', name: name)
+      end
+    end
+  end
+end
