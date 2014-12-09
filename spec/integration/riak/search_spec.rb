6759ab689648e6e0a3916c89a9b5aae10528ba38
Bryce Kerley <bkerley@brycekerley.net>
2014-12-08 21:14:29 -0500

    fill out more search integration specs

--- spec/integration/riak/search_spec.rb
+++ spec/integration/riak/search_spec.rb
@@ -28,12 +28,48 @@ describe 'Object-oriented Search API', test_client: true, integration: true do
   end
 
   describe 'indexes' do
-    it 'tests for index existence and content'
-    it 'creates indexes'
+    it 'tests for index existence and content' do
+      existing_index = Riak::Search::Index.new test_client, index_name
+      expect(existing_index).to be_exists # auto predicate matcher
+
+      nonexistent_index = Riak::Search::Index.new(test_client, 
+                                                  random_key('nonexist'))
+      expect(nonexistent_index).to_not be_exists
+    end
+
+    it 'creates indexes' do
+      new_index = Riak::Search::Index.new test_client, random_key('search_spec')
+      expect(new_index).to_not be_exist
+      expect{ new_index.create! }.to_not raise_error
+
+      wait_until{ new_index.exists? }
+
+      expect(new_index).to be_exists
+
+      expect{ new_index.create! }.to raise_error Riak::SearchError::IndexExistsError
+    end
   end
 
   describe 'schemas' do
-    it 'tests for schema existence and content'
-    it 'creates schemas'
+    it 'tests for schema existence and content' do
+      existing_schema = Riak::Search::Schema.new test_client, '_yz_default'
+      expect(existing_schema).to be_exists
+
+      nonexistent_schema = Riak::Search::Schema.new(test_client,
+                                                    random_key('nonexist'))
+      expect(nonexistent_schema).to_not be_exists
+    end
+
+    it 'creates schemas' do
+      new_schema = Riak::Search::Schema.new test_client, random_key('search_spec')
+      expect(new_schema).to_not be_exist
+      expect{ new_schema.create! }.to_not raise_error
+
+      wait_until{ new_schema.exists? }
+
+      expect(new_schema).to be_exists
+
+      expect{ new_schema.create! }.to raise_error Riak::SearchError::SchemaExistsError
+    end
   end
 end
