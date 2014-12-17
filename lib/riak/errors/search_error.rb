877cd3ebc4f71967c20363817cae20abc4d1c8c8
Bryce Kerley <bkerley@brycekerley.net>
2014-12-17 15:10:13 -0500

    fix search error typos

--- lib/riak/errors/search_error.rb
+++ lib/riak/errors/search_error.rb
@@ -1,9 +1,9 @@
-requrie 'riak/errors/base'
+require 'riak/errors/base'
 
 module Riak
   class SearchError < Error
     class IndexExistsError < SearchError
-      def initialize(nane)
+      def initialize(name)
         super t('search.index_exists', name: name)
       end
     end
