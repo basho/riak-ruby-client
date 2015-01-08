276720aff215e5a8419d80601aade8a2de37279d
Bryce Kerley <bkerley@brycekerley.net>
2015-01-08 13:29:23 -0500

    build out more unit specs on bucket properties objects

--- spec/riak/bucket_properties_spec.rb
+++ spec/riak/bucket_properties_spec.rb
@@ -1,8 +1,40 @@
 describe Riak::BucketProperties do
-  it 'is initialized with a client and bucket'
-  it 'initialzies correctly with a bucket-typed bucket'
+  let(:client){ instance_double 'Riak::Client' }
+  let(:backend) do
+    instance_double('Riak::Client::BeefcakeProtobuffsBackend').tap do |be|
+      allow(client).to receive(:backend).and_yield be
+    end
+  end
 
-  it 'provides hash-like access to properties'
+  it 'is initialized with a client and bucket' do
+    p = nil
+    expect{ p = described_class.new client, bucket }.to_not raise_error
+    expect(p.client).to eq client
+    expect(p.bucket).to eq bucket
+  end
+
+  it 'initialzies correctly with a bucket-typed bucket' do
+    p = nil
+    expect{ p = described_class.new client, typed_bucket }.to_not raise_error
+    expect(p.client).to eq client
+    expect(p.bucket).to eq typed_bucket
+  end
+
+  it 'provides hash-like access to properties' do
+    expect(backend).to receive(:get_bucket_props).
+      with(bucket).
+      and_return('allow_mult' => true)
+
+    expect(subject['allow_mult']).to be
+
+    subject['allow_mult'] = false
+
+    expect(backend).to receive(:set_bucket_props).
+      with(bucket, hash_including(allow_mult: false))
+
+    subject.store
+  end
+
   it 'merges properties from hashes'
   it 'merges properties from other bucket properties objects'
 
