9721a4e013af575f173e81660cdfa261a8e6370b
Bryce Kerley <bkerley@brycekerley.net>
2015-01-16 16:43:39 -0500

    throw out some specs for refactoring bucket properties in the backend

---
+++ spec/riak/beefcake_protobuffs_backend/bucket_properties_operator_spec.rb
@@ -0,0 +1,27 @@
+describe Riak::Client::BeefcakeProtobuffsBackend::BucketPropertiesOperator do
+
+  it 'is initialized with a backend'
+
+  it 'passes through scalar properties'
+
+  describe 'quorums' do
+    it 'normalizes'
+    it 'denormalizes'
+  end
+
+  describe 'commit hooks' do
+    it 'normalizes modfuns'
+    it 'denormalizes modfuns'
+
+    it 'handles names'
+  end
+
+  describe 'modfuns' do
+    it 'normalizes'
+    it 'denormalizes'
+  end
+
+  describe 'repl modes' do
+    it 'denormalizes symbols'
+  end
+end
