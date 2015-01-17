89914856811a0f9129bf761f66751fa66710a59e
Bryce Kerley <bkerley@brycekerley.net>
2015-01-16 19:57:24 -0500

    stub out some hypothetical bucket props operator features

--- spec/riak/beefcake_protobuffs_backend/bucket_properties_operator_spec.rb
+++ spec/riak/beefcake_protobuffs_backend/bucket_properties_operator_spec.rb
@@ -2,10 +2,43 @@ describe Riak::Client::BeefcakeProtobuffsBackend::BucketPropertiesOperator do
 
   it 'is initialized with a backend'
 
-  it 'passes through scalar properties'
+  it 'passes through scalar properties' do
+    expect(protocol).to receive(:write).
+      with(:GetBucketReq, request)
+
+    expect(protocol).to receive(:expect).
+      with(:GetBucketResp).
+      and_return(get_bucket_response)
+
+    resp = nil
+    expect{ resp = subject.get bucket }.to_not raise_error
+
+    expect(resp['n_val']).to eq 3
+  end
+
+  # "normalization" converts from riak naming to ruby-client naming
+  # and quorums from strings into numbers
 
   describe 'quorums' do
-    it 'normalizes'
+    it 'normalizes' do
+      expect(protocol).to receive(:write).
+        with(:GetBucketReq, request)
+
+      expect(protocol).to receive(:expect).
+        with(:GetBucketResp).
+        and_return(get_bucket_response)
+
+      resp = nil
+      expect{ resp = subject.get bucket }.to_not raise_error
+
+      expect(resp['pr']).to eq 'one'
+      expect(resp['r']).to eq 'quorum'
+      expect(resp['w']).to eq 'all'
+      expect(resp['pw']).to eq 'default'
+      expect(resp['dw']).to eq 0
+      expect(resp['rw']).to eq 1
+    end
+
     it 'denormalizes'
   end
 
