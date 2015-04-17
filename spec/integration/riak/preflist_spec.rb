fbfa35a7ce027bad4eef19d6dd94c3e46ef18f47
Bryce Kerley <bkerley@brycekerley.net>
2015-04-17 12:39:06 -0400

    preflist spec

---
+++ spec/integration/riak/preflist_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require 'riak'
+
+describe 'Preflist', integration: true, test_client: true do
+  let(:bucket){ random_bucket }
+  let(:robject) do
+    bucket.get_or_new(random_key).tap do |robj|
+      robj.data = 'asdf'
+      robj.store
+    end
+  end
+
+  matcher :be_a_preflist do
+    match do |actual|
+      actual.is_a?(Array) &&
+      actual.first.is_a?(Riak::PreflistItem)
+    end
+  end
+
+  it 'is available from RObjects' do
+    expect(robject.preflist).to be_a_preflist
+  end
+
+  it 'is available from Buckets' do
+    expect(bucket.preflist robject.key).to be_a_preflist
+  end
+
+  it 'is available from the Client' do
+    expect(test_client.preflist bucket.name, robject.key).to be_a_preflist
+  end
+end
