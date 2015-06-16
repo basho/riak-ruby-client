3e02c00e202581ace97980786a7336a77d4008b5
Bryce Kerley <bkerley@brycekerley.net>
2015-06-16 15:36:13 -0400

    spec out 2i with weird encodings

--- spec/integration/riak/encodings_spec.rb
+++ spec/integration/riak/encodings_spec.rb
@@ -23,11 +23,12 @@ describe 'Encodings', integration: true, test_client: true do
   end
 
   describe 'key-value operations' do
-    it 'allows utf-8 strings in bucket and key names and values' do
+    it 'allows utf-8 strings in bucket and key names, values, and 2i' do
       expect(utf8_bucket).to be_a Riak::Bucket
       expect(robj = utf8_bucket.new(random_utf8_string)).to be_a Riak::RObject
       robj.content_type = 'text/plain'
       robj.data = random_utf8_string
+      robj.indexes['cat_bin'] = [random_utf8_string, 'asdf']
       expect{ robj.store }.to_not raise_error
 
       expect(robj2 = utf8_bucket.get(random_utf8_string)).to be_a Riak::RObject
@@ -37,6 +38,14 @@ describe 'Encodings', integration: true, test_client: true do
       robj.store
       robj2.reload
       expect(robj2.raw_data).to eq random_utf8_string
+      expect(robj2.indexes['cat_bin']).to include 'asdf'
+      expect(robj2.indexes['cat_bin']).
+        to include random_utf8_string.force_encoding('binary')
+
+      expect(utf8_bucket.get_index 'cat_bin', 'asdf').
+        to include random_utf8_string.force_encoding('binary')
+      expect(utf8_bucket.get_index 'cat_bin', random_utf8_string).
+        to include random_utf8_string.force_encoding('binary')
 
       # riak gives us binary-encoding back, which is working as intended
       expect(utf8_bucket.keys).
@@ -49,6 +58,7 @@ describe 'Encodings', integration: true, test_client: true do
         to be_a Riak::RObject
       robj.content_type = 'text/plain'
       robj.data = random_binary_string
+      robj.indexes['cat_bin'] = [random_binary_string, 'asdf']
       expect{ robj.store }.to_not raise_error
 
       expect(robj2 = binary_bucket.get(random_binary_string)).
@@ -59,6 +69,11 @@ describe 'Encodings', integration: true, test_client: true do
       robj.store
       robj2.reload
       expect(robj2.raw_data).to eq random_binary_string
+
+      expect(binary_bucket.get_index 'cat_bin', 'asdf').
+        to include random_binary_string
+      expect(binary_bucket.get_index 'cat_bin', random_binary_string).
+        to include random_binary_string
 
       expect(binary_bucket.keys).to include random_binary_string
     end
