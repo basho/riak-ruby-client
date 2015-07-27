1e1ad0ea13a8f8d1ace2a6e544abdd76d105e2cc
Bryce Kerley <bkerley@brycekerley.net>
2015-07-27 10:59:32 -0400

    fix query syntax

--- spec/integration/riak/time_series_spec.rb
+++ spec/integration/riak/time_series_spec.rb
@@ -6,15 +6,16 @@ describe 'Time Series', test_client: true, integration: true do
     describe 'creating collections' do
 
       let(:table_name){  "time_series_" + random_key }
-      let(:query_text){ <<-SQL }
-create table #{ table_name } (
+      let(:query_text){ <<-SQL.lines.map(&:strip).join ' ' }
+create table tablename (
 time timestamp not null,
 user_id varchar not null,
 temperature_k float,
-partition key (time))
+primary key (time))
 SQL
 
       it 'round-trips a create request without error' do
+pp query_text
         query = Riak::TimeSeries::Query.new test_client, query_text
         expect{ query.issue! }.to_not raise_error
       end
