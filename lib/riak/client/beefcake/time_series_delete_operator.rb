df7849c47fbb1c70b582cd2daf52f205688b587b
Bryce Kerley <bkerley@brycekerley.net>
2015-10-26 19:18:19 -0400

    start time series delete operator

---
+++ lib/riak/client/beefcake/time_series_delete_operator.rb
@@ -0,0 +1,24 @@
+require './ts_cell_codec'
+
+class Riak::Client::BeefcakeProtobuffsBackend
+  def time_series_delete_operator
+    TimeSeriesDeleteOperator.new(self)
+  end
+
+  class TimeSeriesDeleteOperator < Operator
+    def delete(table_name, key_components, options = {})
+      codec = TsCellCodec.new
+      key = key_components.map{ |c| codec.cell_for c }
+
+      request_options = options.merge(table: table_name,
+                                      key: key)
+
+      request = TsDelReq.new request_options
+
+      backend.protocol do |p|
+        p.write :TsDelReq, request
+        p.expect :TsDelResp, TsDelResp, empty_body_acceptable: true
+      end
+    end
+  end
+end
