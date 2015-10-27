d2c0627a603dd9c97de154967fe9269f339e87cb
Bryce Kerley <bkerley@brycekerley.net>
2015-10-27 18:05:04 -0400

    more time series spec passage

--- lib/riak/time_series/deletion.rb
+++ lib/riak/time_series/deletion.rb
@@ -2,9 +2,9 @@ module Riak::TimeSeries
 
   # Delete entries from Riak Time Series.
   class Deletion
-    attr_accessor :keys
+    attr_accessor :key
     attr_accessor :options
-    
+
     attr_reader :client
     attr_reader :table_name
 
@@ -17,7 +17,7 @@ module Riak::TimeSeries
     def delete!
       client.backend do |be|
         be.time_series_delete_operator.delete(table_name,
-                                              keys,
+                                              key,
                                               options)
       end
       true
