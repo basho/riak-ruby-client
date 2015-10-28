790dceb1b919c5b6254282c5b5c11e279ff119f9
Bryce Kerley <bkerley@brycekerley.net>
2015-10-28 14:34:15 -0400

    more complete time series integration specs

--- spec/integration/riak/time_series_spec.rb
+++ spec/integration/riak/time_series_spec.rb
@@ -2,40 +2,70 @@ require 'spec_helper'
 require 'riak'
 
 describe 'Time Series', test_client: true, integration: true do
-  describe 'query interface' do
-    describe 'creating collections' do
+  let(:table_name){ 'GeoCheckin' }
 
-      let(:table_name){  "time_series" }
-      let(:query_text){ <<-SQL.lines.map(&:strip).join ' ' }
-create table #{ table_name } (
-time timestamp not null,
-user_id varchar not null,
-temperature_k float,
-primary key (time))
+  let(:now){ Time.now }
+  let(:now_range_str) do
+    past = (now.to_i - 100) * 1000
+    future = (now.to_i + 100) * 1000
+    "time > #{ past } AND time < #{ future }"
+  end
+
+  let(:family){ 'family-' + random_key }
+  let(:series){ 'series-' + random_key }
+
+  let(:key){ [family, series, now] }
+  let(:datum){ [*key, 'cloudy', 27.1] }
+
+  let(:family_series_str) do
+    "myfamily = '#{family}' AND myseries = '#{series}'"
+  end
+
+  let(:query) do
+    <<-SQL
+SELECT * FROM #{table_name}
+WHERE
+  #{family_series_str} AND
+  #{now_range_str}
 SQL
+  end
 
-      it 'round-trips a create request without error' do
-        query = Riak::TimeSeries::Query.new test_client, query_text
-        expect{ query.issue! }.to_not raise_error
-      end
+  let(:stored_datum_expectation) do
+    submission = Riak::TimeSeries::Submission.new test_client, table_name
+    submission.measurements = [datum]
+    expect{ submission.write! }.to_not raise_error
+  end
+
+  describe 'query interface' do
+    subject{ Riak::TimeSeries::Query.new test_client, query }
+
+    it 'queries data without error' do
+      stored_datum_expectation
+
+      expect{ subject.issue! }.to_not raise_error
+      expect(subject.results).to be
     end
   end
 
-  describe 'submission interface' do
-    let(:table_name){ "time_series" }
-    subject{ Riak::TimeSeries::Submission.new test_client, table_name }
+  describe 'single-key get interface' do
+    subject{ Riak::TimeSeries::Read.new test_client, table_name }
+    it 'retrieves data without error' do
+      stored_datum_expectation
 
-    let(:sample_data) do
-      [
-        [Time.now - 10, 'bryce', 305.37],
-        [Time.now - 5, 'bryce', 300.12],
-        [Time.now, 'bryce', 295.95],
-      ]
+      subject.key = key
+      result = nil
+      expect{ result = subject.read! }.to_not raise_error
+      expect(result).to be
     end
+  end
 
+  describe 'single-key delete interface' do
+    it 'deletes data without error'
+  end
+
+  describe 'submission interface' do
     it 'writes data without error' do
-      subject.measurements = sample_data
-      expect{ subject.write! }.to_not raise_error
+      stored_datum_expectation
     end
   end
 end
