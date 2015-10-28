b062adf8c432d25dce5e0da6cae44db7cf05f8f7
Bryce Kerley <bkerley@brycekerley.net>
2015-10-28 14:34:49 -0400

    use the codec loop in the delete operator

--- lib/riak/client/beefcake/time_series_delete_operator.rb
+++ lib/riak/client/beefcake/time_series_delete_operator.rb
@@ -9,10 +9,9 @@ class Riak::Client::BeefcakeProtobuffsBackend
   class TimeSeriesDeleteOperator < Operator
     def delete(table_name, key_components, options = {})
       codec = TsCellCodec.new
-      key = key_components.map{ |c| codec.cell_for c }
 
       request_options = options.merge(table: table_name,
-                                      key: key)
+                                      key: codec.cells_for(key_components))
 
       request = TsDelReq.new request_options
 
