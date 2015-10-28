c9f99acd8a06242e0692ba5fde2a3d1a1a1fd208
Bryce Kerley <bkerley@brycekerley.net>
2015-10-28 11:55:13 -0400

    Test TsCell deserialization, DRY out specs

--- spec/riak/beefcake_protobuffs_backend/ts_cell_codec_spec.rb
+++ spec/riak/beefcake_protobuffs_backend/ts_cell_codec_spec.rb
@@ -5,22 +5,25 @@ require 'time'
 Riak::Client::BeefcakeProtobuffsBackend.configured?
 
 describe Riak::Client::BeefcakeProtobuffsBackend::TsCellCodec do
-  describe 'serializing values' do
-    it { is_expected.to serialize("hello", binary_value: "hello") }
-    it { is_expected.to serialize(5, integer_value: 5) }
-    it { is_expected.to serialize(123.45, double_value: 123.45) }
-    it { is_expected.to serialize((2**64),
+  describe 'symmetric serialziation' do
+    it { is_expected.to symmetric_serialize("hello", binary_value: "hello")}
+    it { is_expected.to symmetric_serialize(5, integer_value: 5)}
+    it { is_expected.to symmetric_serialize(123.45, double_value: 123.45) }
+    it { is_expected.to symmetric_serialize((2**64),
                                   numeric_value: "18446744073709551616") }
+    it do
+      is_expected.to symmetric_serialize(Time.parse("June 23, 2015 at 9:46:28 EDT"),
+                                         timestamp_value: 1435067188000)
+    end
+    it { is_expected.to symmetric_serialize(true, boolean_value: true) }
+    it { is_expected.to symmetric_serialize(false, boolean_value: false) }
+    it { is_expected.to symmetric_serialize(nil, {}) }
+  end
+
+  describe 'serializing values' do
     it do
       is_expected.to serialize(BigDecimal.new("0.1"), numeric_value: "0.1E0")
     end
-    it do
-      is_expected.to serialize(Time.parse("June 23, 2015 at 9:46:28 EDT"),
-                               timestamp_value: 1435067188000)
-    end
-    it { is_expected.to serialize(true, boolean_value: true) }
-    it { is_expected.to serialize(false, boolean_value: false) }
-    it { is_expected.to serialize(nil, {}) }
 
     it 'refuses to serialize complex numbers' do
       expect{ subject.cell_for(Complex(1, 1)) }.
@@ -33,8 +36,26 @@ describe Riak::Client::BeefcakeProtobuffsBackend::TsCellCodec do
     end
   end
 
-  describe 'deserializing values'
+  # these are handled by the symmetric cases above
+  # describe 'deserializing values'
 
+  RSpec::Matchers.define :symmetric_serialize do |scalar, cell_options|
+    match do |codec|
+      expect(codec).to(
+        serialize(scalar, cell_options)
+        .and(deserialize(scalar, cell_options)))
+    end
+
+    failure_message do |codec|
+      cell = Riak::Client::BeefcakeProtobuffsBackend::TsCell.new cell_options
+      deserialized = codec.scalar_for cell
+      "expected #{scalar} => #{cell_options} => #{scalar}, got #{scalar} => #{cell.to_hash} => #{deserialized}"
+    end
+
+    description do
+      "serialize #{scalar.class} #{scalar.inspect} to and from TsCell #{cell_options}"
+    end
+  end
 
   RSpec::Matchers.define :serialize do |measure, options|
     match do |actual|
@@ -49,6 +70,25 @@ describe Riak::Client::BeefcakeProtobuffsBackend::TsCellCodec do
 
     description do
       "serialize #{measure.class} #{measure.inspect} to TsCell #{options}"
+    end
+  end
+
+  RSpec::Matchers.define :deserialize do |expected, options|
+
+    cell = Riak::Client::BeefcakeProtobuffsBackend::TsCell.new options
+
+    match do |codec|
+      deserialized = codec.scalar_for cell
+      deserialized == expected
+    end
+
+    failure_message do |codec|
+      deserialized = codec.scalar_for cell
+      "expected TsCell #{options.inspect} to deserialize to #{expected.class} #{expected.inspect}"
+    end
+
+    description do
+      "deserialize TsCell #{options} to #{expected.class} #{expected.inspect}"
     end
   end
 end
